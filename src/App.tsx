f4e17a44b68cbd2f7fbe9e191ff757970121c0b2
aline-doleron <doleron.aline@gmail.com>
2024-01-27 17:47:43 -0300

    Add input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import style from './App.module.css';
+import { Rocket, PlusCircle } from "@phosphor-icons/react";
 
 function App() {
   return (
-    <>
+    <div>
       <header className={style.header}>
-        <div><img></img><span className={style.do}>to</span><span className={style.to}>do</span></div>
+        <div className={style.appName}>
+          <Rocket className={style.rocket} size={32} /><span className={style.do}>to</span><span className={style.to}>do</span></div>
       </header>
-    </>
+      <div className={style.wrapper}>
+        <div className={style.inputWrapper}>
+          <input type="text" className={style.input} placeholder='Adicione uma nova tarefa' />
+          <button className={style.createButton}><span>Criar</span><PlusCircle size={24} ></PlusCircle></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
