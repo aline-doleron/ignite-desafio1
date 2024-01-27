046f23ce6ae3c6f176198c58f7a393d2c5240bde
aline-doleron <doleron.aline@gmail.com>
2024-01-27 16:08:46 -0300

    Create header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import style from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <header className={style.header}>
+        <div><img></img><span className={style.do}>to</span><span className={style.to}>do</span></div>
+      </header>
+    </>
+  )
+}
+
+export default App
